6364d0536e6fca37eef530e8712f8fce02144692
AnkitSharma5304 <ankit676756@gmail.com>
2025-06-06 00:09:53 +0530

    Made some changes to improve user experience

--- Tic Tac Toe with 2 modes/script.js
+++ Tic Tac Toe with 2 modes/script.js
@@ -8,7 +8,7 @@ const mylabel=document.querySelector('.mylebel');
 let currentPlayer = 'X';
 let vsComputer = false;
 let gameActive = true;
-
+mylabel.style.display='none';
 const winConditions = [
   [0,1,2], [3,4,5], [6,7,8],
   [0,3,6], [1,4,7], [2,5,8],
